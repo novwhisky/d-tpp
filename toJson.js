5f4f01ef09a4ce9bcc4122e6f3408ad5ec31a344
Nathan Witt <wittnl@gmail.com>
2017-03-01 03:38:22 -0600

    Fixes flatten() to properly structure airport array without the extra city level

--- toJson.js
+++ toJson.js
@@ -13,42 +13,44 @@ const writer = new Writer('./outdata');
 
 
 function flatten(data) {
+    let airports = [];
     const stateObj = data.$;
 
-    const locations = data.city_name.map((city, i) => {
+    data.city_name.map((city, i) => {
         const cityObj = city.$;
-        const airports = city.airport_name.map(airport => {
-            const airportObj = airport.$;
-            const records = airport.record.map(record => {
+
+        airports = airports.concat(
+            city.airport_name.map(airport => {
+                const airportObj = airport.$;
+                const records = airport.record.map(record => {
+                    return {
+                        chartseq: record.chartseq,
+                        chart_code: record.chart_code,
+                        chart_name: record.chart_name,
+                        pdf_name: record.pdf_name
+                    }
+                });
+
                 return {
-                    chartseq: record.chartseq,
-                    chart_code: record.chart_code,
-                    chart_name: record.chart_name,
-                    pdf_name: record.pdf_name
+                    records,
+                    meta: {
+                        state_code: stateObj.ID,
+                        state_fullname: stateObj.state_fullname,
+                        city_name: cityObj.ID,
+                        airport_name: airportObj.ID,
+                        military: airportObj.military,
+                        apt_ident: airportObj.apt_ident,
+                        icao_ident: airportObj.icao_ident,
+                        alnum: airportObj.alnum
+                    }
                 }
-            });
-
-            //console.log(++i + ')', airportObj.apt_ident);
-
-            return {
-                records,
-                meta: {
-                    state_code: stateObj.ID,
-                    state_fullname: stateObj.state_fullname,
-                    city_name: cityObj.ID,
-                    airport_name: airportObj.ID,
-                    military: airportObj.military,
-                    apt_ident: airportObj.apt_ident,
-                    icao_ident: airportObj.icao_ident,
-                    alnum: airportObj.alnum
-                }
-            }
-        });
-
-        return airports.reduce((acc, val) => [].concat(acc, val));
+            })
+        );
     });
 
-    return locations;
+    //airports.map(a => console.log(a.meta.apt_ident + ' ' + a.meta.airport_name));
+
+    return airports;
 }
 
 
