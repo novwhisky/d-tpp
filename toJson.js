71f688aed0d08111669f8554137f7191982a72b5
Nathan Witt <wittnl@gmail.com>
2017-03-01 01:33:02 -0600

    Flatten stream objects into per-state arrays of locations

--- toJson.js
+++ toJson.js
@@ -4,29 +4,59 @@ const fs = require('fs'),
     Writer = require('./lib/json-writer');
 
 
-//fs.readFile('./srcdata/d-TPP_Metafile.xml', (err, data) => {
-//    console.log('!' + data);
-//});
 
-//const xmlStream = fs.createReadStream('./srcdata/d-TPP_Metafile.xml', { defaultEncoding: 'utf8' })
-//    xmlStream.on('data', data => {
-//       console.log('!!! ' + data);
-//    });
-
-//xml.on('startElement: airport_name', data => {
-//    console.log(data);
-//});
-
-//xml.on('endElement: airport_name', data => {
-//    console.log(data);
-//});
-
-//const stream = fs.createReadStream('./srcdata/d-TPP_Metafile.xml');
-const stream = fs.createReadStream('./srcdata/dtpp-sample.xml');
+const stream = fs.createReadStream('./srcdata/d-TPP_Metafile.xml');
+//const stream = fs.createReadStream('./srcdata/dtpp-sample.xml');
 const xml = new XmlStream(stream);
 const writer = new Writer('./outdata');
 
+
+function flatten(data) {
+    const stateObj = data.$;
+
+    const locations = data.city_name.map((city, i) => {
+        const cityObj = city.$;
+        const airports = city.airport_name.map(airport => {
+            const airportObj = airport.$;
+            const records = airport.record.map(record => {
+                return {
+                    chartseq: record.chartseq,
+                    chart_code: record.chart_code,
+                    chart_name: record.chart_name,
+                    pdf_name: record.pdf_name
+                }
+            });
+
+            //console.log(++i + ')', airportObj.apt_ident);
+
+            return {
+                records,
+                meta: {
+                    state_code: stateObj.ID,
+                    state_fullname: stateObj.state_fullname,
+                    city_name: cityObj.ID,
+                    airport_name: airportObj.ID,
+                    military: airportObj.military,
+                    apt_ident: airportObj.apt_ident,
+                    icao_ident: airportObj.icao_ident,
+                    alnum: airportObj.alnum
+                }
+            }
+        });
+
+        return airports;
+    });
+
+    return locations;
+}
+
+
+xml.collect('city_name');
+xml.collect('airport_name');
 xml.collect('record');
 xml.on('endElement: state_code', data => {
-    writer.write(data);
+    //console.log(data);
+    //writer.write(data);
+    var d = flatten(data);
 });
+
