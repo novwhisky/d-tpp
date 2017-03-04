4c49742591f0efa24e4241bc8410c223342dc82e
Nathan Witt <wittnl@gmail.com>
2017-03-04 00:48:17 -0600

    Add tpp metadata & template updates

--- toJson.js
+++ toJson.js
@@ -12,52 +12,58 @@ const xml = new XmlStream(stream);
 const writer = new Writer('./web');
 
 
-function flatten(data) {
+function flatten(tpp) {
     let airports = [];
-    const stateObj = data.$;
 
-    data.city_name.map(city => {
-        const cityObj = city.$;
+    const tppObj = tpp.$;
 
-        airports = airports.concat(
-            city.airport_name.map(airport => {
-                const airportObj = airport.$;
-                const records = airport.record.map(record => {
+    tpp.state_code.map(state => {
+        const stateObj = state.$;
+
+        state.city_name.map(city => {
+            const cityObj = city.$;
+
+            airports = airports.concat(
+                city.airport_name.map(airport => {
+                    const airportObj = airport.$;
+                    const records = airport.record.map(record => {
+                        return {
+                            chartseq: record.chartseq,
+                            chart_code: record.chart_code,
+                            chart_name: record.chart_name,
+                            pdf_name: record.pdf_name
+                        }
+                    });
+
                     return {
-                        chartseq: record.chartseq,
-                        chart_code: record.chart_code,
-                        chart_name: record.chart_name,
-                        pdf_name: record.pdf_name
+                        records,
+                        meta: {
+                            state_code: stateObj.ID,
+                            state_fullname: stateObj.state_fullname,
+                            city_name: cityObj.ID,
+                            airport_name: airportObj.ID,
+                            military: airportObj.military,
+                            apt_ident: airportObj.apt_ident,
+                            icao_ident: airportObj.icao_ident,
+                            alnum: airportObj.alnum,
+                            cycle: tppObj.cycle,
+                            from_edate: tppObj.from_edate,
+                            to_edate: tppObj.to_edate
+                        }
                     }
-                });
-
-                return {
-                    records,
-                    meta: {
-                        state_code: stateObj.ID,
-                        state_fullname: stateObj.state_fullname,
-                        city_name: cityObj.ID,
-                        airport_name: airportObj.ID,
-                        military: airportObj.military,
-                        apt_ident: airportObj.apt_ident,
-                        icao_ident: airportObj.icao_ident,
-                        alnum: airportObj.alnum
-                    }
-                }
-            })
-        );
+                })
+            );
+        });
     });
-
-    //airports.map(a => console.log(a.meta.apt_ident + ' ' + a.meta.airport_name));
 
     return airports;
 }
 
-
+xml.collect('state_code');
 xml.collect('city_name');
 xml.collect('airport_name');
 xml.collect('record');
-xml.on('endElement: state_code', data => {
+xml.on('endElement: digital_tpp', data => {
     var d = flatten(data);
     writer.write(d);
 });
